740c845fc248d829e7341a3cb7b389f0c078770c
vansh sharma <vs6905397@gmail.com>
2026-09-09 13:40:19 +0530

    tree practice

---
+++ Tree/symmetricTree.js
@@ -0,0 +1,35 @@
+//question:https://leetcode.com/problems/symmetric-tree/ 
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    function check(left, right){
+        if(left === null && right === null){
+            return true;
+        }
+
+        if(left === null || right === null){
+            return false;
+        }
+
+        if(left.val !== right.val){
+            return false;
+        }
+
+
+        return check(left.left, right.right) &&
+        check(left.right, right.left);
+    }
+
+    return check(root.left, root.right)
+};
