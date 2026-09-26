e65269473993042e19c2bc9dceb95ff3b23888ec
vansh sharma <vs6905397@gmail.com>
2026-09-26 12:27:04 +0530

    heap practice

---
+++ Heap/topKFrequentElement.js
@@ -0,0 +1,114 @@
+//question: https://leetcode.com/problems/top-k-frequent-elements/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let  freq = new Map();
+
+    for(let num of nums){
+        freq.set(num, (freq.get(num) || 0) +1);
+    };
+
+    let minHeap = new Minheap();
+
+    for(let [num, count] of freq){
+        minHeap.insert([num, count]);
+
+        if(minHeap.size() > k){
+            minHeap.extractMin();
+        }
+
+    }
+
+return minHeap.heap.map(([num,count]) => num);
+};
+
+ class Minheap{
+    constructor(){
+        this.heap = [];
+    }
+
+    insert(value){
+        this.heap.push(value);
+        this.heapifyUp();
+    }
+
+    heapifyUp(){
+        let index = this.heap.length - 1;
+
+        while(index > 0){
+            let parent = Math.floor((index-1)/2);
+
+            if(this.heap[index][1] >= this.heap[parent][1]){
+                break;
+            }
+
+            [this.heap[index], this.heap[parent]] = [this.heap[parent], this.heap[index]];
+
+            index = parent;
+        }
+    }
+
+    extractMin(){
+
+        if(this.heap.length === 0){
+            return null;
+        }
+
+        if(this.heap.length === 1){
+            return this.heap.pop();
+        }
+
+        let min = this.heap[0];
+
+        let last = this.heap.pop();
+
+        this.heap[0] = last;
+
+        this.heapifyDown();
+
+        return min;
+    }
+
+    heapifyDown(){
+        let index = 0;
+
+        while(true){
+            let left = 2*index+1;
+            let right = 2*index+2;
+
+            let smallest = index;
+
+            if(left < this.heap.length && this.heap[left][1] < this.heap[smallest][1]){
+                smallest = left;
+            }
+
+            if(right < this.heap.length && this.heap[right][1] < this.heap[smallest][1]){
+                smallest = right;
+            }
+
+            if(smallest === index){
+                break;
+            }
+
+            [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+
+            index = smallest;
+        }
+    }
+
+    peek(){
+        return this.heap[0];
+    }
+
+    size(){
+        return this.heap.length;
+    }
+
+    isEmpty(){
+        return this.heap.length === 0;
+    }
+}
